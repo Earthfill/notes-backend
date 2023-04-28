3cc2e9a109d4251caeae665e73f9272f2d11e3d1
Kingsley Omoaka-Iyorah <kingsley.omiy@gmail.com>
2023-04-28 07:07:11 +0100

    testing

--- index.js
+++ index.js
@@ -46,31 +46,27 @@ app.get('/api/notes/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
-  }
+  Note.findOne({ content: body.content })
+    .then(existingNote => {
+      if (existingNote) {
+        return response.status(409).json({ error: 'Note already exists' })
+      }
 
-  Note.findOne({ content: body.content }).then(note => {
-    if (note) {
-      return response.status(409).json({
-        error: 'Note already exists in the database'
-      })
-    } else {
       const note = new Note({
         content: body.content,
         important: body.important || false,
       })
 
-      note.save().then(savedNote => {
+    note.save()
+      .then(savedNote => {
         response.json(savedNote)
       })
-    }
+      .catch(error => next(error))
   })
+  .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
@@ -82,7 +78,7 @@ app.put('/api/notes/:id', (request, response, next) => {
     important: body.important,
   }
 
-  Note.findByIdAndUpdate(id, note, { new: true })
+  Note.findByIdAndUpdate(id, note, { new: true, runValidators: true, context: 'query' })
     .then(updatedNote => {
       response.json(updatedNote)
     })
@@ -111,7 +107,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
